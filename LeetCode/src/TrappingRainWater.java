ff055e5d5cca2fbe32225343698d08f4b75828fe
acprimer <729101421@qq.com>
2014-11-26 01:30:28 +0800

    Trapping Rain Water

--- LeetCode/src/TrappingRainWater.java
+++ LeetCode/src/TrappingRainWater.java
@@ -1,36 +1,52 @@
 /**
  * Created by yaodh on 2014/11/25.
+ * <p/>
+ * LeetCode: Trapping Rain Water
+ * Link: https://oj.leetcode.com/problems/trapping-rain-water/
+ * Description:
+ * -----------------------------
+ * Given n non-negative integers representing an elevation map where the width of each bar is 1,
+ * compute how much water it is able to trap after raining.
+ * For example,
+ * Given [0,1,0,2,1,0,1,3,2,1,2,1], return 6.
+ * -----------------------------
+ * <p/>
+ * Tag: Binary Search
  */
 public class TrappingRainWater {
     public int trap(int[] A) {
         if (A.length <= 1) return 0;
-        int preMax = A[0];
         int ans = 0;
-        for (int i = 1; i < A.length; i++) {
-            while (i < A.length && A[i] >= A[i - 1]) {
-                preMax = A[i];
-                i++;
-            }
-            int j = i;
-            while (j < A.length && A[j] <= A[j - 1]) {
-                ans += preMax - A[j];
-                j++;
-            }
-            while (j < A.length && A[j] >= A[j - 1] && A[j] <= preMax) {
-                ans += preMax - A[j];
-                j++;
-            }
-            if (A[j - 1] < preMax) {
-                ans -= (j - i) * (preMax - A[j - 1]);
-                preMax = A[j - 1];
-            }
-            i = j - 1;
+        int p = 1, q = A.length - 2;
+        while (p < A.length && A[p] >= A[p - 1]) {
+            p++;
         }
+        while (q >= 0 && A[q] >= A[q + 1]) {
+            q--;
+        }
+        p--;
+        q++;
+        int sum = 0;
+        for (int i = p + 1; i < q; i++) {
+            int minHeight = Math.min(A[p], A[q]);
+            if (A[i] <= minHeight) {
+                sum += A[i];
+            } else {
+                ans += (i - p - 1) * Math.min(A[p], A[i]) - sum;
+                sum = 0;
+                p = i;
+            }
+        }
+        if (q - p > 1) ans += (q - p - 1) * Math.min(A[p], A[q]) - sum;
         return ans;
     }
 
     public static void main(String[] args) {
-        int ans = new TrappingRainWater().trap(new int[]{0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1});
+        int ans = new TrappingRainWater().trap(new int[]{6,4,2,0,3,2,0,3,1,4,5,3,2,7,5,3,0,1,2,1,3,4,6,8,1,3});
         System.out.println(ans);
     }
 }
+// 5,2,1,2,1,5 14
+// 5,4,1,2  1
+// 0,2,0 0
+// 6,4,2,0,3,2,0,3,1,4,5,3,2,7,5,3,0,1,2,1,3,4,6,8,1,3 -->83
