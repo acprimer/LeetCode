5ffe4e89d271c2cfdb52347662d370527013b49a
acprimer <729101421@qq.com>
2014-11-27 01:01:51 +0800

    Combination Sum

--- LeetCode/src/CombinationSumII.java
+++ LeetCode/src/CombinationSumII.java
@@ -5,54 +5,60 @@ import java.util.List;
 /**
  * Created by yaodh on 2014/11/27.
  * <p/>
- * LeetCode: Combination Sum
- * Link: https://oj.leetcode.com/problems/combination-sum/
+ * LeetCode: Combination Sum II
+ * Link: https://oj.leetcode.com/problems/combination-sum-ii/
  * Description:
  * -----------------------------
- * Given a set of candidate numbers (C) and a target number (T),
+ * Given a collection of candidate numbers (C) and a target number (T),
  * find all unique combinations in C where the candidate numbers sums to T.
- * The same repeated number may be chosen from C unlimited number of times.
+ * Each number in C may only be used once in the combination.
  * Note:
  * All numbers (including target) will be positive integers.
  * Elements in a combination (a1, a2, … , ak) must be in non-descending order. (ie, a1 ≤ a2 ≤ … ≤ ak).
  * The solution set must not contain duplicate combinations.
- * For example, given candidate set 2,3,6,7 and target 7,
+ * For example, given candidate set 10,1,2,7,6,1,5 and target 8,
  * A solution set is:
- * [7]
- * [2, 2, 3]
+ * [1, 7]
+ * [1, 2, 5]
+ * [2, 6]
+ * [1, 1, 6]
  * -----------------------------
  * <p/>
  * Tag: Backtracking
  */
-public class CombinationSum {
+public class CombinationSumII {
     List<List<Integer>> ans = new ArrayList<List<Integer>>();
 
-    public List<List<Integer>> combinationSum(int[] candidates, int target) {
-        Arrays.sort(candidates);
-        dfs(candidates, 0, 0, target, new ArrayList<Integer>());
+    public List<List<Integer>> combinationSum2(int[] num, int target) {
+        Arrays.sort(num);
+        dfs(num, 0, 0, target, new ArrayList<Integer>());
         return ans;
     }
 
-    private void dfs(int[] candidates, int start, int sum, int target, List<Integer> list) {
+    private void dfs(int[] num, int start, int sum, int target, List<Integer> list) {
         if (sum == target) {
             ans.add(list);
             return;
         }
-        if (start >= candidates.length || sum > target || sum + candidates[start] > target) {
+        if (start >= num.length || sum > target || sum + num[start] > target) {
             return;
         }
+        int j = start;
+        while (j < num.length && num[j] == num[start]) {
+            j++;
+        }
         List<Integer> preList = new ArrayList<Integer>(list);
-        dfs(candidates, start + 1, sum, target, preList);
-        while (sum <= target) {
+        dfs(num, j, sum, target, preList);
+        for (int i = start; i < j; i++) {
             List<Integer> newList = new ArrayList<Integer>(preList);
-            newList.add(candidates[start]);
+            newList.add(num[start]);
             preList = newList;
-            dfs(candidates, start + 1, sum += candidates[start], target, newList);
+            dfs(num, j, sum += num[start], target, newList);
         }
     }
 
     public static void main(String[] args) {
-        List<List<Integer>> ans = new CombinationSum().combinationSum(new int[]{1, 2, 3, 6, 7}, 7);
+        List<List<Integer>> ans = new CombinationSumII().combinationSum2(new int[]{10, 1, 2, 7, 6, 1, 5}, 8);
         for (List<Integer> line : ans) {
             for (int x : line) {
                 System.out.print(x + " ");
