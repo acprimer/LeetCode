2af3d0c2eec44ea7ad6c3f8717d18125a2aa73ac
acprimer <729101421@qq.com>
2014-12-03 00:38:56 +0800

    Wildcard Matching

--- LeetCode/src/RegularExpressionMatching.java
+++ LeetCode/src/RegularExpressionMatching.java
@@ -1,37 +1,104 @@
 /**
- * Created by yao on 2014/11/30.
+ * Created by yaodh on 2014/1/2.
+ * <p/>
+ * LeetCode: Regular Expression Matching
+ * Link: https://oj.leetcode.com/problems/regular-expression-matching/
+ * Description:
+ * -----------------------------
+ * Implement regular expression matching with support for '.' and '*'.
+ * '.' Matches any single character.
+ * '*' Matches zero or more of the preceding element.
+ * The matching should cover the entire input string (not partial).
+ * The function prototype should be:
+ * bool isMatch(const char *s, const char *p)
+ * Some examples:
+ * isMatch("aa","a") → false
+ * isMatch("aa","aa") → true
+ * isMatch("aaa","aa") → false
+ * isMatch("aa", "a*") → true
+ * isMatch("aa", ".*") → true
+ * isMatch("ab", ".*") → true
+ * isMatch("aab", "c*a*b") → true
+ * -----------------------------
+ * <p/>
+ * Tag: Backtracking DP
  */
 public class RegularExpressionMatching {
-    public boolean isMatch(String s, String p) {
-        return match(s, p, 0, 0, false);
+    /*public boolean isMatch(String s, String p) {
+        boolean[][] dp = new boolean[p.length()+1][s.length()+1];
+        for(int i=0;i<=p.length();i++) {
+            dp[i][0] = true;
+        }
+        for(int i=0;i<=s.length();i++) {
+            dp[0][i] = true;
+        }
+        int match = 0;
+        for(int i=0;i<p.length();i++) {
+            // single character
+            if(i+1>=p.length() || p.charAt(i+1)!='*') {
+                for(int j=match;j<s.length();j++) {
+                    dp[i+1][j+1] = dp[i][j] && match(p.charAt(i), s.charAt(j));
+                }
+                match++;
+                continue;
+            }
+            // .*
+            if(i+1<p.length() && p.substring(i,i+2).equals(".*")) {
+                for(int j=match;j<s.length();j++) {
+                    dp[i+1][j+1] = true;
+                }
+                continue;
+            }
+            // a*
+            if(i+1<p.length() && p.charAt(i+1)=='*') {
+                if(dp[i][match])
+                for(int j=match;j<s.length();j++) {
+                    if(p.charAt(i)!=s.charAt(j)) break;
+                    dp[i+1][j+1] = dp[i][j];
+                }
+                continue;
+            }
+        }
     }
 
-    private boolean match(String s, String p, int sIndex, int pIndex, boolean flag) {
-        if (pIndex >= p.length()) {
-            if (!flag && sIndex < s.length()) return false;
-            return true;
-        }
-        for (int i = 0; i + pIndex < p.length(); i++) {
-            if (p.charAt(i + pIndex) == '*') {
-                for (int j = i - 1; j >= 0; j--) {
-                    if (j + sIndex < 0 || j + sIndex >= s.length()
-                            || (p.charAt(j + pIndex) != '.' && p.charAt(j + pIndex) != s.charAt(j + sIndex))) {
-                        return false;
+    public boolean match(char a, char b){
+        if(a=='.' || a==b) return true;
+        return false;
+    }*/
+
+    // Backtracking
+    public boolean isMatch(String s, String p) {
+        if (p.length() <= 0) return s.equals(p);
+        if (p.length() > 1 && p.charAt(1) == '*') {
+            if (p.charAt(0) == '.') {
+                for (int i = 0; i <= s.length(); i++) {
+                    if (isMatch(s.substring(i), p.substring(2))) {
+                        return true;
                     }
                 }
-                return match(s, p, i + sIndex, i + pIndex + 1, true);
-            }
-            if (flag) continue;
-            if (i + sIndex >= s.length()
-                    || (p.charAt(i + pIndex) != '.' && p.charAt(i + pIndex) != s.charAt(i + sIndex))) {
                 return false;
             }
+            if (isMatch(s, p.substring(2))) {
+                return true;
+            }
+            for (int i = 0; i < s.length(); i++) {
+                if (s.charAt(i) == p.charAt(0)) {
+                    if (isMatch(s.substring(i + 1), p.substring(2))) {
+                        return true;
+                    }
+                } else break;
+            }
+        } else {
+            if (s.length() >= 1 &&
+                    (p.charAt(0) == '.' || s.charAt(0) == p.charAt(0))) {
+                return isMatch(s.substring(1), p.substring(1));
+            } else return false;
         }
-        return s.length() == p.length();
+        return false;
     }
 
     public static void main(String[] args) {
-        boolean ans = new RegularExpressionMatching().isMatch("a", ".*..a*");
+        boolean ans = new RegularExpressionMatching().isMatch("", ".*");
         System.out.println(ans);
     }
 }
--- LeetCode/src/WildcardMatching.java
+++ LeetCode/src/WildcardMatching.java
@@ -2,7 +2,65 @@
  * Created by yaodh on 2014/11/25.
  */
 public class WildcardMatching {
+
     public boolean isMatch(String s, String p) {
-        return true;
+        s = ' ' + s;
+        p = ' ' + p;
+        boolean[][] dp = new boolean[p.length()][s.length()];
+        dp[0][0] = true;
+        int last = 0;
+        boolean flag = false;
+        for (int i = 1; i < p.length(); i++) {
+            if (p.charAt(i) != '*') {
+                if(flag) {
+                    int tmp = -1;
+                    for (int j = last + 1; j < s.length(); j++) {
+                        dp[i][j] = dp[i - 1][j - 1] && match(p.charAt(i), s.charAt(j));
+                        if (dp[i][j] && tmp == -1) {
+                            tmp = j;
+                        }
+                    }
+                    if (tmp == -1) return false;
+                    last = tmp;
+                }
+                else{
+                    last++;
+                    if(s.length()<=last) return false;
+                    dp[i][last] = dp[i - 1][last - 1] && match(p.charAt(i), s.charAt(last));
+                    if(dp[i][last]) return false;
+                }
+                flag = false;
+            } else {
+                flag = true;
+            }
+        }
+        return dp[p.length() - 1][s.length() - 1];
+    }
+
+    public boolean match(char a, char b) {
+        if (a == '?' || a == b) return true;
+        return false;
+    }
+
+    // TLE
+    /*public boolean isMatch(String s, String p) {
+        if (p.length() <= 0) return s.equals(p);
+        if (p.charAt(0) == '*') {
+            for (int i = 0; i <= s.length(); i++) {
+                if (isMatch(s.substring(i), p.substring(1))) {
+                    return true;
+                }
+            }
+            return false;
+        }
+        if (s.length() >= 1 && match(p.charAt(0), s.charAt(0)) && isMatch(s.substring(1), p.substring(1))) {
+            return true;
+        }
+        return false;
+    }*/
+
+    public static void main(String[] args) {
+        boolean ans = new WildcardMatching().isMatch("a", "aa");
+        System.out.println(ans);
     }
 }
