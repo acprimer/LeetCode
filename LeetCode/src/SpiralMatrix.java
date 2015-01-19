613db6d6e2bcfd73f421a415e896b927c54f6efc
acprimer <729101421@qq.com>
2015-01-19 21:50:49 +0800

    Spiral Matrix

--- LeetCode/src/SpiralMatrix.java
+++ LeetCode/src/SpiralMatrix.java
@@ -12,9 +12,9 @@ import java.util.List;
  * For example,
  * Given the following matrix:
  * [
- *  [ 1, 2, 3 ],
- *  [ 4, 5, 6 ],
- *  [ 7, 8, 9 ]
+ * [ 1, 2, 3 ],
+ * [ 4, 5, 6 ],
+ * [ 7, 8, 9 ]
  * ]
  * You should return [1,2,3,6,9,8,7,4,5].
  * -----------------------------
@@ -45,8 +45,44 @@ public class SpiralMatrix {
         return ans;
     }
 
+    public List<Integer> spiralOrder2(int[][] matrix) {
+        int rows = matrix.length;
+        if (rows <= 0) {
+            return new ArrayList<Integer>(0);
+        }
+        int cols = matrix[0].length;
+        List<Integer> ans = new ArrayList<Integer>(rows * cols);
+        for (int i = 0; i < (rows + 1) / 2; i++) {
+            for (int j = i; j < cols - i; j++) {
+                ans.add(matrix[i][j]);
+            }
+            if (ans.size() == rows * cols) {
+                break;
+            }
+            for (int j = i + 1; j < rows - i; j++) {
+                ans.add(matrix[j][cols - i - 1]);
+            }
+            if (ans.size() == rows * cols) {
+                break;
+            }
+            for (int j = cols - i - 2; j >= i; j--) {
+                ans.add(matrix[rows - i - 1][j]);
+            }
+            for (int j = rows - i - 2; j > i; j--) {
+                ans.add(matrix[j][i]);
+            }
+        }
+        return ans;
+    }
+
     public static void main(String[] args) {
-        List<Integer> ans = new SpiralMatrix().spiralOrder(new int[][]{{1}});
+        List<Integer> ans = new SpiralMatrix().spiralOrder2(new int[][]{{1}, {2}, {3}, {4}, {5}, {6}, {7}, {8}, {9}});
+        System.out.println(ans);
+        ans = new SpiralMatrix().spiralOrder2(new int[][]{{1, 2, 3, 4, 5, 6, 7, 8, 9}});
+        System.out.println(ans);
+        ans = new SpiralMatrix().spiralOrder2(new int[][]{{1, 2, 3}, {4, 5, 6}, {7, 8, 9}});
+        System.out.println(ans);
+        ans = new SpiralMatrix().spiralOrder2(new int[][]{{1, 2}, {3, 4}, {5, 6}, {7, 8}});
         System.out.println(ans);
     }
 }
