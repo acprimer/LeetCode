5292282a67e0ad58ba7b44fede701f2b8cd53c64
acprimer <729101421@qq.com>
2015-01-21 21:54:36 +0800

    Set Matrix Zeroes

--- LeetCode/src/ContainerWithMostWater.java
+++ LeetCode/src/ContainerWithMostWater.java
@@ -15,23 +15,39 @@
  * Tag: TwoPointers
  */
 public class ContainerWithMostWater {
+//    public int maxArea(int[] height) {
+//        if (height.length <= 1) return 0;
+//        int ans = 0;
+//        int first = 0, last = height.length - 1;
+//        while (first < last) {
+//            if (height[first] < height[last]) {
+//                ans = Math.max(ans, height[first] * (last - first));
+//                int h = height[first];
+//                while (first < last && height[first] <= h) {
+//                    first++;
+//                }
+//            } else {
+//                ans = Math.max(ans, height[last] * (last - first));
+//                int h = height[last];
+//                while (first < last && height[last] <= h) {
+//                    last--;
+//                }
+//            }
+//        }
+//        return ans;
+//    }
+
     public int maxArea(int[] height) {
-        if (height.length <= 1) return 0;
         int ans = 0;
-        int first = 0, last = height.length - 1;
-        while (first < last) {
-            if (height[first] < height[last]) {
-                ans = Math.max(ans, height[first] * (last - first));
-                int h = height[first];
-                while (first < last && height[first] <= h) {
-                    first++;
-                }
-            } else {
-                ans = Math.max(ans, height[last] * (last - first));
-                int h = height[last];
-                while (first < last && height[last] <= h) {
-                    last--;
-                }
+        int left = 0, right = height.length - 1;
+        while (left < right) {
+            int h = Math.min(height[left], height[right]);
+            ans = Math.max(ans, (right - left) * h);
+            while (left < right && height[left] <= h) {
+                left++;
+            }
+            while (left < right && height[right] <= h) {
+                right--;
             }
         }
         return ans;
--- LeetCode/src/SetMatrixZeroes.java
+++ LeetCode/src/SetMatrixZeroes.java
@@ -11,26 +11,73 @@
  * Tag: Array
  */
 public class SetMatrixZeroes {
+    // O(m+n) space
+//    public void setZeroes(int[][] matrix) {
+//        if (matrix == null || matrix.length <= 0) return;
+//        int row = matrix.length;
+//        int column = matrix[0].length;
+//        boolean[] zeroRows = new boolean[row];
+//        boolean[] zeroColumns = new boolean[column];
+//        for (int i = 0; i < row; i++) {
+//            for (int j = 0; j < column; j++) {
+//                if (matrix[i][j] == 0) {
+//                    zeroRows[i] = true;
+//                    zeroColumns[j] = true;
+//                }
+//            }
+//        }
+//        for (int i = 0; i < row; i++) {
+//            for (int j = 0; j < column; j++) {
+//                if (zeroRows[i] || zeroColumns[j]) {
+//                    matrix[i][j] = 0;
+//                }
+//            }
+//        }
+//    }
+
+    // O(1) space
     public void setZeroes(int[][] matrix) {
         if (matrix == null || matrix.length <= 0) return;
-        int row = matrix.length;
-        int column = matrix[0].length;
-        boolean[] zeroRows = new boolean[row];
-        boolean[] zeroColumns = new boolean[column];
-        for (int i = 0; i < row; i++) {
-            for (int j = 0; j < column; j++) {
+        int row = -1, col = -1;
+        int n = matrix.length, m = matrix[0].length;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
                 if (matrix[i][j] == 0) {
-                    zeroRows[i] = true;
-                    zeroColumns[j] = true;
+                    if (row == -1) {
+                        row = i;
+                        col = j;
+                    } else {
+                        matrix[row][j] = matrix[i][col] = 0;
+                    }
                 }
             }
         }
-        for (int i = 0; i < row; i++) {
-            for (int j = 0; j < column; j++) {
-                if (zeroRows[i] || zeroColumns[j]) {
+        if (row == -1) {
+            return;
+        }
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (i != row && j != col && (matrix[row][j] == 0 || matrix[i][col] == 0)) {
                     matrix[i][j] = 0;
                 }
             }
+        }
+        for (int i = 0; i < n; i++) {
+            matrix[i][col] = 0;
+        }
+        for (int i = 0; i < m; i++) {
+            matrix[row][i] = 0;
+        }
+    }
+
+    public static void main(String[] args) {
+        int[][] matrix = new int[][]{{1, 0, 2}, {1, 1, 1}, {0, 1, 1}};
+        new SetMatrixZeroes().setZeroes(matrix);
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                System.out.printf("%d ", matrix[i][j]);
+            }
+            System.out.println();
         }
     }
 }
