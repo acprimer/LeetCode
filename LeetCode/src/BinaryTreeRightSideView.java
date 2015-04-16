c5cfa596d94e0a1442079396d9c8c7d7acb3d1f1
acprimer <729101421@qq.com>
2015-04-16 20:12:31 +0800

    Binary Tree Right Side View

--- LeetCode/src/BinaryTreeRightSideView.java
+++ LeetCode/src/BinaryTreeRightSideView.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Queue;
 
@@ -17,9 +18,28 @@ public class BinaryTreeRightSideView {
     }
     public List<Integer> rightSideView(TreeNode root) {
         List<Integer> ans = new ArrayList<Integer>();
-        ans.add(root.val);
-        int length = 1;
-        for(int i=0;i<length;i++) {
+        if(root == null) {
+            return ans;
+        }
+        List<TreeNode> cur = new LinkedList<TreeNode>();
+        cur.add(root);
+        List<TreeNode> next = new LinkedList<TreeNode>();
+        while(cur.size() > 0) {
+            int val = cur.get(0).val;
+            if(cur.get(0).left != null) {
+                next.add(cur.get(0).left);
+            }
+            if(cur.get(0).right != null) {
+                next.add(cur.get(0).right);
+            }
+            cur.remove(0);
+            if(cur.size() == 0) {
+                ans.add(val);
+                List<TreeNode> tmp = cur;
+                cur = next;
+                next = tmp;
+            }
+
         }
         return ans;
     }
