914c0dd89624654262f097c02279b4dd40d36afa
acprimer <729101421@qq.com>
2017-12-19 22:51:53 +0800

    LongestWordInDictionary

---
+++ LeetCode/src/ReverseString.java
@@ -0,0 +1,8 @@
+/**
+ * Created by yaodh on 2017/12/18.
+ */
+public class ReverseString {
+    public String reverseString(String s) {
+        return new StringBuilder(s).reverse().toString();
+    }
+}
